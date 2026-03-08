a8dcd05ce4b4867035657e8d8b54650952fc2734
yogipowar <powaryogesh20@gmail.com>
2026-03-08 14:39:42 +0530

    Add gh-pages support and update Vite configuration
    - Added "gh-pages" as a dependency for deployment.
    - Updated package.json scripts for predeploy and deploy commands.
    - Simplified Vite configuration by removing unnecessary babel plugin settings.

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react({
-      babel: {
-        plugins: [['babel-plugin-react-compiler']],
-      },
-    }),
-  ],
-})
+  base: "/chatbot-app/",
+  plugins: [react()],
+});
