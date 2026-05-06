e93a26bf1784154f9b5f32771001a447d0917da3
Yogesh Powar <yogesh.onpoint@gmail.com>
2026-05-06 14:13:13 +0530

    fix: Uncomment website ID retrieval in Chatbot component for dynamic usage

--- src/components/Chatbot.jsx
+++ src/components/Chatbot.jsx
@@ -137,9 +137,9 @@ function Chatbot() {
 
   useEffect(() => {
     const params = new URLSearchParams(window.location.search);
-    // const id = params.get("websiteId");
+    const id = params.get("websiteId");
 
-    const id = "97"
+    // const id = "97"
 
     console.log("Website ID:", id);
 
